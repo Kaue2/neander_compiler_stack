cd930ad00b5dbb686b057826854c60e3efaf02aa
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-03-22 01:18:33 -0300

    add: criar enum para tipos de token

--- src/assembler.rs
+++ src/assembler.rs
@@ -1,1 +1,28 @@
+enum TokenType {
+    // Delimitadores Iniciadores
+    TokenSetup,
+    TokenData,
+    TokenInicio,
+
+    // Delimitadores Fim
+    TokemFim,
+
+    // Variaveis
+    TokenVar,
+
+    // Instrucoes
+    TokenInstrucaoSetUp,
+    TokenInstrucao,
+
+    // Literais
+    TokenNum,
+
+    // Simbolos unicos
+    TokenIgual,
+    TokenDoisPontos,
+    TokenSeta,
+}
+
+pub struct Token {}
+
 fn main() {}
